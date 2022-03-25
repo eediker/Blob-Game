d08cf7ce117d10635301149917785a31ac73d9e1
Enes Eyüp Diker <eediker@outlook.com>
2022-03-25 15:21:27 +0300

    Admin and User classes has been added to project with default username and password

--- Program.cs
+++ Program.cs
@@ -19,4 +19,50 @@ namespace OOP_LAB1
             Application.Run(new Form1());
         }
     }
+
+    public class Admin
+    {
+        private string username;
+        public string password;
+        Admin(string username = "admin",string password = "admin")
+        {
+            Username = username;
+            Password = password;
+        }
+
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+    }
+    public class User
+    {
+        private string username;
+        public string password;
+        User(string username = "user", string password = "user")
+        {
+            Username = username;
+            Password = password;
+        }
+
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+    }
 }
