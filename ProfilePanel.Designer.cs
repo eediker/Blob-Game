a453fbf4a3a87199d14cd3068db29c71062c8796
Enes Eyüp Diker <eediker@outlook.com>
2022-04-09 05:16:36 +0300

    Profile panel is added to project

---
+++ ProfilePanel.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace OOP_LAB1
+{
+    partial class ProfilePanel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.EmailField = new System.Windows.Forms.TextBox();
+            this.CountryField = new System.Windows.Forms.TextBox();
+            this.CityField = new System.Windows.Forms.TextBox();
+            this.AddressField = new System.Windows.Forms.TextBox();
+            this.PhoneNumberField = new System.Windows.Forms.TextBox();
+            this.NameSurnameField = new System.Windows.Forms.TextBox();
+            this.PasswordField = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.PasswordCheck = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Save = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(134, 72);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(529, 40);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // EmailField
+            // 
+            this.EmailField.Location = new System.Drawing.Point(257, 273);
+            this.EmailField.Name = "EmailField";
+            this.EmailField.Size = new System.Drawing.Size(123, 20);
+            this.EmailField.TabIndex = 70;
+            // 
+            // CountryField
+            // 
+            this.CountryField.Location = new System.Drawing.Point(503, 238);
+            this.CountryField.Name = "CountryField";
+            this.CountryField.Size = new System.Drawing.Size(123, 20);
+            this.CountryField.TabIndex = 69;
+            // 
+            // CityField
+            // 
+            this.CityField.Location = new System.Drawing.Point(503, 199);
+            this.CityField.Name = "CityField";
+            this.CityField.Size = new System.Drawing.Size(123, 20);
+            this.CityField.TabIndex = 68;
+            // 
+            // AddressField
+            // 
+            this.AddressField.Location = new System.Drawing.Point(503, 166);
+            this.AddressField.Name = "AddressField";
+            this.AddressField.Size = new System.Drawing.Size(123, 20);
+            this.AddressField.TabIndex = 67;
+            // 
+            // PhoneNumberField
+            // 
+            this.PhoneNumberField.Location = new System.Drawing.Point(257, 237);
+            this.PhoneNumberField.Name = "PhoneNumberField";
+            this.PhoneNumberField.Size = new System.Drawing.Size(123, 20);
+            this.PhoneNumberField.TabIndex = 66;
+            // 
+            // NameSurnameField
+            // 
+            this.NameSurnameField.Location = new System.Drawing.Point(257, 205);
+            this.NameSurnameField.Name = "NameSurnameField";
+            this.NameSurnameField.Size = new System.Drawing.Size(123, 20);
+            this.NameSurnameField.TabIndex = 65;
+            // 
+            // PasswordField
+            // 
+            this.PasswordField.Location = new System.Drawing.Point(257, 166);
+            this.PasswordField.Name = "PasswordField";
+            this.PasswordField.Size = new System.Drawing.Size(123, 20);
+            this.PasswordField.TabIndex = 64;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(153, 276);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 62;
+            this.label8.Text = "E-mail";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(432, 245);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(43, 13);
+            this.label7.TabIndex = 61;
+            this.label7.Text = "Country";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(435, 206);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(24, 13);
+            this.label6.TabIndex = 60;
+            this.label6.Text = "City";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(432, 166);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 13);
+            this.label5.TabIndex = 59;
+            this.label5.Text = "Address";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(151, 240);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 13);
+            this.label4.TabIndex = 58;
+            this.label4.Text = "Phone Number";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(151, 208);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 57;
+            this.label3.Text = "Name-Surname";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(153, 169);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 56;
+            this.label2.Text = "Password";
+            // 
+            // PasswordCheck
+            // 
+            this.PasswordCheck.Location = new System.Drawing.Point(320, 359);
+            this.PasswordCheck.Name = "PasswordCheck";
+            this.PasswordCheck.Size = new System.Drawing.Size(123, 20);
+            this.PasswordCheck.TabIndex = 71;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(317, 333);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 13);
+            this.label1.TabIndex = 72;
+            this.label1.Text = "Enter the current password";
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(345, 401);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(75, 23);
+            this.Save.TabIndex = 73;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = true;
+            // 
+            // ProfilePanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PasswordCheck);
+            this.Controls.Add(this.EmailField);
+            this.Controls.Add(this.CountryField);
+            this.Controls.Add(this.CityField);
+            this.Controls.Add(this.AddressField);
+            this.Controls.Add(this.PhoneNumberField);
+            this.Controls.Add(this.NameSurnameField);
+            this.Controls.Add(this.PasswordField);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "ProfilePanel";
+            this.Text = "ProfilePanel";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox EmailField;
+        private System.Windows.Forms.TextBox CountryField;
+        private System.Windows.Forms.TextBox CityField;
+        private System.Windows.Forms.TextBox AddressField;
+        private System.Windows.Forms.TextBox PhoneNumberField;
+        private System.Windows.Forms.TextBox NameSurnameField;
+        private System.Windows.Forms.TextBox PasswordField;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox PasswordCheck;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Save;
+    }
+}
