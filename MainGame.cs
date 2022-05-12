55d675e6a7ec56635efdd35285aaf2a9c6f2ce7f
Enes Eyüp Diker <eediker@outlook.com>
2022-05-12 22:48:08 +0300

    Update MainGame.cs

--- MainGame.cs
+++ MainGame.cs
@@ -12,6 +12,7 @@ namespace OOP_LAB1
 {
     public partial class MainGame : Form
     {
+        int[,] matrix = new int[SettingsSave.Default.Height, SettingsSave.Default.Width];
         int[] ButtonInfo = new int[SettingsSave.Default.Width * SettingsSave.Default.Height];
         Button[] btn = new Button[SettingsSave.Default.Width * SettingsSave.Default.Height];
         bool flag; string Remember; int score = 0;
@@ -44,21 +45,28 @@ namespace OOP_LAB1
             }
             else if (ButtonInfo[int.Parse(btn_now.Name)] == 0 && flag == true)
             {
-                 btn[int.Parse(btn_now.Name)].Image = Image.FromFile("../../images/" + ButtonInfo[int.Parse(Remember)] + ".jpg");
-                 ButtonInfo[int.Parse(btn_now.Name)] = ButtonInfo[int.Parse(Remember)];
-                 ButtonInfo[int.Parse(Remember)] = 0;
-                 btn[int.Parse(Remember)].Image = default;
-                 flag = false;
-                 Random3Objects();
-                 Remember = "0";
+
+                MakeaMove(btn_now);
+
+                flag = false;
+                Random3Objects();
+                Remember = "0";
             }
-            CheckIsOver();
+            if (CheckIsOver())
+            {
+                RestartGame();
+            }
         }
 
         private void SettingsButton_Click(object sender, EventArgs e)
         {
             var Settings = new Settings();
+            string remember = SettingsSave.Default.Diffuculty;
             Settings.ShowDialog();
+            if(SettingsSave.Default.Diffuculty != remember)
+            {
+                Application.Restart();
+            }
         }
 
         private void AdminPanelButton_Click(object sender, EventArgs e)
@@ -90,18 +98,21 @@ namespace OOP_LAB1
             while (x > 0)
             {
                 Random rd = new Random();
-                int rand_num1 = rd.Next(1, 9);
+                int rand_num1 = rd.Next(1, 10);
                 int rand_num2 = rd.Next(0, SettingsSave.Default.Width * SettingsSave.Default.Height);
                 if (ButtonInfo[rand_num2] == 0)
                 {
                     btn[rand_num2].Image = Image.FromFile("../../images/" + rand_num1.ToString() + ".jpg");
                     ButtonInfo[rand_num2] = rand_num1;
+                    int height = rand_num2 / SettingsSave.Default.Width;
+                    int width = rand_num2 % SettingsSave.Default.Height;
+                    matrix[height, width] = rand_num1;
                     x--;
                 }
             }
         }
 
-        void CheckIsOver()
+        bool CheckIsOver()
         {
 
             int i = SettingsSave.Default.Width * SettingsSave.Default.Height-1;
@@ -109,17 +120,61 @@ namespace OOP_LAB1
             {
                 if(ButtonInfo[i] == 0)
                 {
-                    return;
+                    return false;
                 };
                 i--;
             }
             MessageBox.Show("GAME IS OVER!!!");
-            StartGame();
-            return;
+            return true;
+        }
+
+        void MakeaMove(Button btn_now)
+        {
+            // Changing the values between previous and last clicked buttons
+            btn[int.Parse(btn_now.Name)].Image = Image.FromFile("../../images/" + ButtonInfo[int.Parse(Remember)] + ".jpg");
+            ButtonInfo[int.Parse(btn_now.Name)] = ButtonInfo[int.Parse(Remember)];
+            ButtonInfo[int.Parse(Remember)] = 0;
+            btn[int.Parse(Remember)].Image = default;
+
+            int height = int.Parse(btn_now.Name) / SettingsSave.Default.Width;
+            int width = int.Parse(btn_now.Name) % SettingsSave.Default.Height;
+
+            matrix[height, width] = ButtonInfo[int.Parse(btn_now.Name)];
+
+            height = int.Parse(Remember) / SettingsSave.Default.Width;
+            width = int.Parse(Remember) % SettingsSave.Default.Height;
+
+            matrix[height, width] = 0;
         }
 
         void StartGame()
         {
+            int number = 0;
+            for (int i = 0; i < SettingsSave.Default.Height; i++)
+            {
+                for (int j = 0; j < SettingsSave.Default.Width; j++)
+                {
+
+                    btn[number] = new Button();
+                    btn[number].Name = number.ToString();
+                    btn[number].Size = new Size(35, 35);
+                    btn[number].Location = new Point(35 * (j + 2), 35 * (i + 2));
+                    btn[number].Click += new EventHandler(ButtonArray_click);
+                    ButtonInfo[number] = 0;
+                    Controls.Add(btn[number]);
+                    matrix[i,j] = 0;
+                    number++;
+                }
+            }
+            Random3Objects();
+        }
+
+        void RestartGame()
+        {
+            matrix = new int[SettingsSave.Default.Height, SettingsSave.Default.Width];
+            ButtonInfo = new int[SettingsSave.Default.Width * SettingsSave.Default.Height];
+            btn = new Button[SettingsSave.Default.Width * SettingsSave.Default.Height];
+            score = 0;
             int number = 0;
             for (int i = 0; i < SettingsSave.Default.Height; i++)
             {
@@ -131,11 +186,22 @@ namespace OOP_LAB1
                     btn[number].Location = new Point(35 * (j + 2), 35 * (i + 2));
                     btn[number].Click += new EventHandler(ButtonArray_click);
                     ButtonInfo[number] = 0;
-                    Controls.Add(btn[number]);
+                    matrix[i, j] = 0;
+                    btn[number].Image = default;
                     number++;
                 }
             }
             Random3Objects();
         }
+        
+        void GiveScore()
+        {
+            if (SettingsSave.Default.Diffuculty == "Easy") score += 1;
+            else if (SettingsSave.Default.Diffuculty == "Normal") score += 3;
+            else if (SettingsSave.Default.Diffuculty == "Hard") score += 5;
+            else if (SettingsSave.Default.Diffuculty == "Custom") score += 2;
+        }
+
     }
+
 }
