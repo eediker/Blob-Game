eb673efd62c4f7eef150f511c6a8af8ff072fafe
kubradurakk <kbradrakk1@gmail.com>
2022-05-23 14:16:19 +0300

    Revert "Revert "Revert "log out button and closing warning"""
    This reverts commit b074b3e92345fc5832aaaecf38519c4bcc29cd53.

--- MainGame.Designer.cs
+++ MainGame.Designer.cs
@@ -36,15 +36,13 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.ShowScore = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // SettingsButton
             // 
-            this.SettingsButton.Location = new System.Drawing.Point(1676, 50);
-            this.SettingsButton.Margin = new System.Windows.Forms.Padding(4);
+            this.SettingsButton.Location = new System.Drawing.Point(1257, 41);
             this.SettingsButton.Name = "SettingsButton";
-            this.SettingsButton.Size = new System.Drawing.Size(159, 28);
+            this.SettingsButton.Size = new System.Drawing.Size(119, 23);
             this.SettingsButton.TabIndex = 0;
             this.SettingsButton.Text = "Settings";
             this.SettingsButton.UseVisualStyleBackColor = true;
@@ -52,10 +50,9 @@
             // 
             // ShowUserProfile
             // 
-            this.ShowUserProfile.Location = new System.Drawing.Point(1676, 15);
-            this.ShowUserProfile.Margin = new System.Windows.Forms.Padding(4);
+            this.ShowUserProfile.Location = new System.Drawing.Point(1257, 12);
             this.ShowUserProfile.Name = "ShowUserProfile";
-            this.ShowUserProfile.Size = new System.Drawing.Size(159, 28);
+            this.ShowUserProfile.Size = new System.Drawing.Size(119, 23);
             this.ShowUserProfile.TabIndex = 1;
             this.ShowUserProfile.Text = "Profile";
             this.ShowUserProfile.UseVisualStyleBackColor = true;
@@ -63,10 +60,9 @@
             // 
             // AdminPanelButton
             // 
-            this.AdminPanelButton.Location = new System.Drawing.Point(1673, 122);
-            this.AdminPanelButton.Margin = new System.Windows.Forms.Padding(4);
+            this.AdminPanelButton.Location = new System.Drawing.Point(1255, 99);
             this.AdminPanelButton.Name = "AdminPanelButton";
-            this.AdminPanelButton.Size = new System.Drawing.Size(161, 28);
+            this.AdminPanelButton.Size = new System.Drawing.Size(121, 23);
             this.AdminPanelButton.TabIndex = 2;
             this.AdminPanelButton.Text = "Admin Panel";
             this.AdminPanelButton.UseVisualStyleBackColor = true;
@@ -74,10 +70,9 @@
             // 
             // AboutScreenButton
             // 
-            this.AboutScreenButton.Location = new System.Drawing.Point(1676, 86);
-            this.AboutScreenButton.Margin = new System.Windows.Forms.Padding(4);
+            this.AboutScreenButton.Location = new System.Drawing.Point(1257, 70);
             this.AboutScreenButton.Name = "AboutScreenButton";
-            this.AboutScreenButton.Size = new System.Drawing.Size(159, 28);
+            this.AboutScreenButton.Size = new System.Drawing.Size(119, 23);
             this.AboutScreenButton.TabIndex = 3;
             this.AboutScreenButton.Text = "About Screen";
             this.AboutScreenButton.UseVisualStyleBackColor = true;
@@ -87,10 +82,9 @@
             // 
             this.ShowBestScore.AutoSize = true;
             this.ShowBestScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.ShowBestScore.Location = new System.Drawing.Point(1689, 235);
-            this.ShowBestScore.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.ShowBestScore.Location = new System.Drawing.Point(1267, 191);
             this.ShowBestScore.Name = "ShowBestScore";
-            this.ShowBestScore.Size = new System.Drawing.Size(137, 58);
+            this.ShowBestScore.Size = new System.Drawing.Size(109, 46);
             this.ShowBestScore.TabIndex = 4;
             this.ShowBestScore.Text = "none";
             // 
@@ -98,10 +92,9 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label1.Location = new System.Drawing.Point(1676, 203);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(1257, 165);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(147, 31);
+            this.label1.Size = new System.Drawing.Size(119, 26);
             this.label1.TabIndex = 5;
             this.label1.Text = "Best Score";
             // 
@@ -109,10 +102,9 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label2.Location = new System.Drawing.Point(1693, 316);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(1270, 257);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(85, 31);
+            this.label2.Size = new System.Drawing.Size(69, 26);
             this.label2.TabIndex = 6;
             this.label2.Text = "Score";
             // 
@@ -120,29 +112,17 @@
             // 
             this.ShowScore.AutoSize = true;
             this.ShowScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.ShowScore.Location = new System.Drawing.Point(1689, 361);
-            this.ShowScore.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.ShowScore.Location = new System.Drawing.Point(1267, 293);
             this.ShowScore.Name = "ShowScore";
-            this.ShowScore.Size = new System.Drawing.Size(137, 58);
+            this.ShowScore.Size = new System.Drawing.Size(109, 46);
             this.ShowScore.TabIndex = 7;
             this.ShowScore.Text = "none";
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(1676, 158);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(158, 23);
-            this.button1.TabIndex = 8;
-            this.button1.Text = "Logout";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
             // MainGame
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1853, 800);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(1390, 650);
             this.Controls.Add(this.ShowScore);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -151,11 +131,9 @@
             this.Controls.Add(this.AdminPanelButton);
             this.Controls.Add(this.ShowUserProfile);
             this.Controls.Add(this.SettingsButton);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "MainGame";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Game";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainGame_FormClosing);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -171,6 +149,5 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label ShowScore;
-        private System.Windows.Forms.Button button1;
     }
 }
